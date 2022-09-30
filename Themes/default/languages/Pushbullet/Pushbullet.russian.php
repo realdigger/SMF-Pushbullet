c0483e282f055f487329a6e7f6f64df82ded8637
digger <digger@mysmf.net>
2022-10-01 00:13:16 +0400

    Add languages.

---
+++ Themes/default/languages/Pushbullet/Pushbullet.russian.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package SMF Pushbullet Notifications
+ * @file Pushbullet.russian.php
+ * @author digger <digger@mysmf.net> <https://mysmf.net>
+ * @copyright Copyright (c) 2017, digger
+ * @license The MIT License (MIT) https://opensource.org/licenses/MIT
+ * @version 1.0
+ */
+
+$txt['pushbullet'] = 'Уведомления Pushbullet';
+$txt['pushbullet_enabled'] = 'Включить уведомления через Pushbullet';
+$txt['pushbullet_api_key'] = 'Ключ доступа Pushbullet';
+$txt['pushbullet_nocurl'] = 'Уведомления не будут работать из-за отсутствия PHP cURL!';
+$txt['pushbullet_nojson'] = 'Уведомления не будут работать из-за отсутствия PHP JSON!';
+$txt['pushbullet_test_ok'] = 'Тест API: успешно';
+$txt['pushbullet_test_false'] = 'Тест API: ошибка';
